808311777009f6e09a4cc215c7d96ef39fb8cfa6
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2022-01-24 00:18:30 +0900

    Renew All
    あまりにも作りが雑であったので全てを作り直した。
    ついでに #1 も解決されている。

--- js/dakutenize.js
+++ js/dakutenize.js
@@ -1,10 +1,29 @@
-const s = document.getElementById('s');
-const b = document.getElementById('b');
+'use strict';
 
-b.addEventListener('click', () => {
-	const org = s.value;
-	const arr = [];
-	for (const c of org.split(''))
-		arr.push(c, '\u3099');
-	s.value = arr.join('');
-});
+const input = document.getElementById('input');
+const topping = document.forms.topping.topping;
+const dakutenize = document.getElementById('dakutenize');
+const output = document.getElementById('output');
+const copy = document.getElementById('copy');
+
+function dakutenizer()
+{
+	const dakutenchars = {
+		'dakuten': '\u3099',	// 合成用の濁点
+		'handaku': '\u309A',	// 合成用の半濁点
+		'maru':    '\u20DD',	// 合成用の丸
+		'shikaku': '\u20DE',	// 合成用の四角
+	};
+	let dakutenchar = dakutenchars[topping.value] || '\u3099';
+
+	output.value = input.value.replace(/\S/g, '$&' + dakutenchar);
+}
+
+function copier()
+{
+	output.select();
+	document.execCommand('copy');
+}
+
+dakutenize.addEventListener('click', dakutenizer);
+copy.addEventListener('click', copier);
